c23b680ac7b48c49ff3f200e3ddefbe5ea44785d
Leonardod <lfdantoni@gmail.com>
2019-10-09 00:05:34 -0300

    update file names config

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,69 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.scss']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader"
-          },
-          {
-            loader: "ts-loader"
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-      },
-      {
-        test: /\.(scss|sass|css)$/,
-        exclude: /node_modules/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                localIdentName: '[folder]__[local]--[hash:base64:5]'
-              }
+
+module.exports = function(env, argv) {
+  const devMode = process.env.NODE_ENV !== 'production' && argv.mode !== 'production';
+
+  return {
+    entry: './src/index.tsx',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.scss']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader"
+            },
+            {
+              loader: "ts-loader"
             }
-          },
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    })
-  ],
-  output: {
-		path: __dirname + "/dist",
-		filename: "main.js"
-	}
-};
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(scss|sass|css)$/,
+          exclude: /node_modules/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  localIdentName: '[folder]__[local]--[hash:base64:5]'
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+        filename: "./index.html"
+      }),
+      new MiniCssExtractPlugin({
+        filename: devMode ? '[name].css' : '[name].[hash].css',
+        chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+      })
+    ],
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].[hash].bundle.js"
+    }
+  }
+}
