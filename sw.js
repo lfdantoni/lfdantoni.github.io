bdf6e46f2bdad92f9357773b5a25c93026171ba4
Leonardod <lfdantoni@gmail.com>
2020-09-28 01:06:08 -0300

    final

--- sw.js
+++ sw.js
@@ -1,4 +1,33 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles.css',
+  '/index.js'
+];
+
 self.addEventListener('install', function(event) {
   // Perform install steps
   console.log('Service Worker: Installed')
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
